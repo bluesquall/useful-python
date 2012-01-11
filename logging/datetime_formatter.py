c4b8b27e7e9b25ee68a39359fc2f10f887ea57f1
M J Stanway <m.j.stanway@alum.mit.edu>
2012-01-11 21:36:48 +0000

    simpler formatTime method for DSLDateTimeFormatter

--- logging/datetime_formatter.py
+++ logging/datetime_formatter.py
@@ -27,9 +27,24 @@ class UTCDatetimeFormatter(DatetimeFormatter):
 
 
 class DSLDatetimeFormatter(UTCDatetimeFormatter):
-    def formatTime(self, record, datefmt="%Y/%m/%d %H:%M:%S.%f"):
-        # ct = self.converter(record.created)
-        # return ct.strftime(datefmt)
-        return self.converter.(record_created).strftime(datefmt)
+    def formatTime(self, record, datefmt=None):
+        ct = self.converter(record.created)
+        # ignore the datefmt kwarg
+        return "%s.%03d" % (ct.strftime("%Y/%m/%d %H:%M:%S"), record.msecs)
+        # TODO new string format
 
+if __name__ == "__main__":
+    mfmt = "%(asctime)s %(levelname)s %(message)s"
+    
+    #formatter = DatetimeFormatter(mfmt, "%Y/%m/%d %H:%M:%S.%f")
+    #formatter = UTCDatetimeFormatter(mfmt)
+    formatter = DSLDatetimeFormatter(mfmt)
 
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    shandler = logging.StreamHandler()
+    shandler.setLevel(logging.DEBUG)
+    shandler.setFormatter(formatter)
+    logger.addHandler(shandler)
+
+    logger.info('How now brown cow?')
